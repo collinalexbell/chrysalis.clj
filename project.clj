f773ac27b0ef2e03a494b192cf6aceb9784478ef
kuberlog <collinalexbell@gmail.com>
2021-04-24 20:27:08 -0500

    project.clj complete

--- project.clj
+++ project.clj
@@ -1,3 +1,8 @@
 (defproject chrysalis "0.0.1"
   :description "A cybernetics platform"
-  :url "https://github.com/kuberlog/chrysalis.clj")
+  :url "https://github.com/kuberlog/chrysalis.clj"
+  :license {:name "GNU GPLv3"
+            :url "https://www.gnu.org/licenses/gpl-3.0.html"}
+  :dependencies []
+  :main chrysalis.core)
+
